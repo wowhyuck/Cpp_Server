86c88f5196351a8cf1c82868be3e41d9e9e7eb56
Joohyuck <joohyuck.seo@gmail.com>
2021-10-07 17:54:12 +0900

    Atomic

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,56 +2,46 @@
 #include <iostream>
 #include "CorePch.h"
 #include <thread>
+#include <atomic>
 
-void HelloThread()
+// atomic : All-Or-Nothing
+atomic<int32> sum = 0;
+
+void Add()
 {
-    cout << "Hello Thread" << endl;
+	for (int32 i = 0; i < 100'0000; i++)
+	{
+		sum.fetch_add(1);
+		//sum++;
+		//int32 eax = sum;
+		//eax = eax + 1;
+		//sum = eax;
+	}
 }
 
-void HelloThread2(int32 num)
+void Sub()
 {
-    cout << num << endl;
+	for (int32 i = 0; i < 100'0000; i++)
+	{
+		sum.fetch_add(-1);
+		//sum--;
+		//int32 eax = sum;
+		//eax = eax - 1;
+		//sum = eax;
+	}
 }
+
 
 int main()
 {
+	Add();
+	Sub();
+	cout << sum << endl;
 
-    // HelloThread
-
-    //std::thread t(HelloThread);
-    std::thread t;
-
-    auto id1 = t.get_id();
-
-    t = std::thread(HelloThread);
-
-    int32 count = t.hardware_concurrency();     // CPU 코어 개수?
-    auto id2 = t.get_id();     // 쓰레드마다 id
-
-    //t.detach();     // std::thread 객체에서 실제 쓰레드를 분리
-
-    if(t.joinable())
-        t.join();
-
-    std::thread t2(HelloThread2, 10);
-
-    t2.join();
-
-    vector<std::thread> v;
-
-    for (int32 i = 0; i < 10; i++)
-    {
-        v.push_back(std::thread(HelloThread2, i));
-    }
-
-    for (int32 i = 0; i < 10; i++)
-    {
-        if (v[i].joinable())
-            v[i].join();
-    }
-
-
-    cout << "Hello Main" << endl;
-
+	std::thread t1(Add);
+	std::thread t2(Sub);
+	t1.join();
+	t2.join();
+	cout << sum << endl;
 
 }
